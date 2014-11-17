d19a0f9242a2ea14afbf0385191b50253764e2b2
unknown <pkusisto@indiana.edu>
2014-11-16 20:46:32 -0500

    Added a quick Python script to create a .csv of all the results for a given generation.

---
+++ Python/condenseRuns.py
@@ -0,0 +1,16 @@
+def condenseRuns(genNumber):
+    """Turns a directory full of runs into one .csv file"""
+    allResults = ["Run Number, Maximum X, Frame Reached\n"]
+    
+    # Find and open the results
+    dir = os.path.dirname(__file__)
+    resultsPath = os.path.join(dir, "../Output/gen" + str(genNumber) + "/Results/")
+    
+    for fileNum, filename in enumerate(os.listdir(resultsPath)):            # Iterate
+        resultFile = open(resultsPath + filename, 'r')                      # Open the file
+        allResults.append(str(fileNum) + ',' + resultFile.readline()+'\n')  # Add the result to our list
+        resultFile.close()                                                  # Close the file
+    
+    outputFile = open(resultsPath + "allResults.csv", 'w')  # Create a .csv
+    outputFile.writelines(allResults)                       # Write the results
+    outputFile.close()                                      # Close the file
