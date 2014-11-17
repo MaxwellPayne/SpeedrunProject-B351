73764cbf4d3a9c32c6bf3dc35596643bf64109d3
unknown <pkusisto@indiana.edu>
2014-11-16 20:49:11 -0500

    Bug fixes for condenseRuns.py

--- Python/condenseRuns.py
+++ Python/condenseRuns.py
@@ -1,3 +1,5 @@
+import os
+
 def condenseRuns(genNumber):
     """Turns a directory full of runs into one .csv file"""
     allResults = ["Run Number, Maximum X, Frame Reached\n"]
@@ -14,3 +16,5 @@ def condenseRuns(genNumber):
     outputFile = open(resultsPath + "allResults.csv", 'w')  # Create a .csv
     outputFile.writelines(allResults)                       # Write the results
     outputFile.close()                                      # Close the file
+
+condenseRuns(0)
