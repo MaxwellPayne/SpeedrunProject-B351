1f24f6c8ef39cc7d210bf4dab1a87ca9b6d7005c
pkusisto <pkusisto@indiana.edu>
2014-11-17 17:48:12 -0500

    Create fileIO.py

---
+++ Python/fileIO.py
@@ -0,0 +1,11 @@
+def makeGen(genNumber,runCount,workerCount = 1):
+"""Creates a new generation with a config file"""
+    pass
+
+def readRun(genNumber,runNumber):
+"""Reads a given run number from a given generation, and returns a list of strings (the input strings)"""
+    pass
+  
+def writeRun(genNumber,runNumber,inputList):
+"""Writes a given run number to a given generation, based on the passed list of strings (the inputs)"""
+    pass
