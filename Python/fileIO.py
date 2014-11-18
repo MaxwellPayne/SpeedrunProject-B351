a4f2c018b5e1ecd176891b1ea69eebe915d2d1aa
unknown <pkusisto@indiana.edu>
2014-11-17 20:18:59 -0500

    Added readRun() and writeRun() to fileIO.py

--- Python/fileIO.py
+++ Python/fileIO.py
@@ -6,14 +6,28 @@ def makeGen(genNumber):
 
 def readRun(genNumber,runNumber):
     """Reads a given run number from a given generation, and returns a list of strings (the input strings)"""
-    pass
-  
-def writeRun(genNumber,runNumber,inputList):
+    # Find the file
+    dir = os.path.dirname(__file__)
+    filename = os.path.join(dir, "../Output/gen" + str(genNumber) + "/Runs/run" + str(runNumber) + ".txt")
+    
+    runFile = open(filename, 'r')           # Open the file
+    lineList = runFile.read().splitlines()  # Read all lines into a list
+    runFile.close()                         # Close the file
+    
+    return lineList
+
+def writeRun(genNumber,runNumber,lineList):
     """Writes a given run number to a given generation, based on the passed list of strings (the inputs)"""
-    pass
+    # Find the file
+    dir = os.path.dirname(__file__)
+    filename = os.path.join(dir, "../Output/gen" + str(genNumber) + "/Runs/run" + str(runNumber) + ".txt")
+    
+    runFile = open(filename, 'w')   # Open the file
+    runFile.writelines(lineList)    # Write the given lines
+    runFile.close()                 # Close the file
 
 def readResult(genNumber,runNumber):
-    """Reads a given result number from a given generation, and returns a list where index 0 is the maximum X, and index 1 is the frame"""
+    """Reads a given result number from a given generation, and returns a list where index 0 is the maximum X, and index 1 is the frame count"""
     # Find and open the results folder
     dir = os.path.dirname(__file__)
     resultsPath = os.path.join(dir, "../Output/gen" + str(genNumber) + "/Results/")
@@ -34,7 +48,7 @@ def readResult(genNumber,runNumber):
     return resultList
 
 def getConfig():
-    """Returns the settings of the configuration file as a list"""
+    """Returns the settings of the configuration file as a list - don't use this if you could use a more specific function, such as getRunsPerGen()"""
     # Temporary hack...
     return [100,1,0]
 
