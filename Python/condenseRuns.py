3a7cb8f753b0f5a1db72b1ca760cd2a0fab6679e
Kendall Ebley <kebley@indiana.edu>
2014-11-17 23:18:39 -0500

    Completed function to return best runs from a generation

--- Python/condenseRuns.py
+++ Python/condenseRuns.py
@@ -1,5 +1,6 @@
 import os
 from fileIO import *
+from operator import itemgetter
 
 def condenseRuns(genNumber):
     """Returns the results of every run in a given generation"""
@@ -13,7 +14,20 @@ def condenseRuns(genNumber):
 
 def getBestRuns(genNumber,numberOfRuns):
     """Returns a list of integers representing the top numberOfRuns runs from the given generation"""
-    pass
+    results = condenseRuns(genNumber)
+    results.sort(key=lambda x: x[2])            # Sort runs based on distance reached
+    results.reverse()                           # Reverse so I can take the top N runs
+    bestRuns = []
+    if numberOfRuns == "completed":
+        for i in results:
+            if i[2] == 2000:                    # Will return only runs that reached the end of the level
+                bestRuns.append(i)
+    else:
+        for i in range(0,numberOfRuns):         # Add run numbers of best runs to list
+            run = results[i]
+            runNum = run[0]
+            bestRuns.append(runNum)
+    return bestRuns
 
 def createCSV():
     return  # This code doesn't work - it's only here in case we decide we want to be able to make .csv files again
@@ -26,3 +40,5 @@ def createCSV():
 
 if __name__ == '__main__':
     print condenseRuns(0)
+    print getBestRuns(0,10)
+
