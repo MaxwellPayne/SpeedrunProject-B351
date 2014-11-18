f9f75586e4fd1481f582272b788c9545f10833f1
unknown <pkusisto@indiana.edu>
2014-11-17 20:03:29 -0500

    Updated condenseRuns()

--- Python/condenseRuns.py
+++ Python/condenseRuns.py
@@ -1,29 +1,28 @@
 import os
+from fileIO import *
 
 def condenseRuns(genNumber):
-    """Turns a directory full of runs into one .csv file"""
-    allResults = ["Run Number, Maximum X, Frame Reached\n"]
-    
-    # Find and open the results
-    dir = os.path.dirname(__file__)
-    resultsPath = os.path.join(dir, "../Output/gen" + str(genNumber) + "/Results/")
-    
-    for fileNum, filename in enumerate(os.listdir(resultsPath)):            # Iterate
-        resultFile = open(resultsPath + filename, 'r')                      # Open the file
-        allResults.append(str(fileNum) + ',' + resultFile.readline()+'\n')  # Add the result to our list
-        resultFile.close()                                                  # Close the file
-    
-    outputFile = open(resultsPath + "../allResults.csv", 'w')  # Create a .csv
-    outputFile.writelines(allResults)                          # Write the results
-    outputFile.close()                                         # Close the file
-    
-    allResults.pop(0)   # Remove the headers before we return
+    """Returns the results of every run in a given generation"""
+    allResults = []
+    for runNumber in range(getRunsPerGen()):             # For ever run in the generation
+        resultList = readResult(genNumber,runNumber)    # Get the result of that run
+        resultList.insert(0,runNumber)                  # Add the run number to it,
+        allResults.append(resultList)                   # Then add it to the list
     return allResults
+
 
 def getBestRuns(genNumber,numberOfRuns):
     """Returns a list of integers representing the top numberOfRuns runs from the given generation"""
     pass
 
+def createCSV():
+    return  # This code doesn't work - it's only here in case we decide we want to be able to make .csv files again
+    
+    allResults = ["Run Number, Maximum X, Frame Reached\n"]
+    
+    outputFile = open(resultsPath + "../allResults.csv", 'w')  # Create a .csv
+    outputFile.writelines(allResults)                          # Write the results
+    outputFile.close()                                         # Close the file
 
 if __name__ == '__main__':
-    condenseRuns(0)
+    print condenseRuns(0)
--- Python/fileIO.py
+++ Python/fileIO.py
@@ -1,11 +1,42 @@
-def makeGen(genNumber,runCount,workerCount = 1):
-"""Creates a new generation with a config file"""
+import os
+
+def makeGen(genNumber):
+    """Creates a new generation"""
     pass
 
 def readRun(genNumber,runNumber):
-"""Reads a given run number from a given generation, and returns a list of strings (the input strings)"""
+    """Reads a given run number from a given generation, and returns a list of strings (the input strings)"""
     pass
   
 def writeRun(genNumber,runNumber,inputList):
-"""Writes a given run number to a given generation, based on the passed list of strings (the inputs)"""
+    """Writes a given run number to a given generation, based on the passed list of strings (the inputs)"""
     pass
+
+def readResult(genNumber,runNumber):
+    """Reads a given result number from a given generation, and returns a list where index 0 is the maximum X, and index 1 is the frame"""
+    # Find and open the results folder
+    dir = os.path.dirname(__file__)
+    resultsPath = os.path.join(dir, "../Output/gen" + str(genNumber) + "/Results/")
+    
+    #Build the path to the requested file
+    fileName = resultsPath + "result" + str(runNumber) + ".txt"
+    
+    # Open the file, and read it to a string, then close the file
+    resultFile = open(fileName, 'r')
+    resultString = resultFile.readline()
+    resultFile.close()
+    
+    # Split the string into a list, and turn the strings to ints
+    resultList = resultString.split(',')
+    resultList = map(int, resultList)
+    
+    # Return the list of ints
+    return resultList
+
+def getConfig():
+    """Returns the settings of the configuration file as a list"""
+    # Temporary hack...
+    return [100,1,0]
+
+def getRunsPerGen():
+    return getConfig()[0]
