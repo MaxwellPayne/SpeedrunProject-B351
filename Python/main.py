59af0537f2bfc75a24faee1e68d31b07d07ab20e
unknown <pkusisto@indiana.edu>
2014-11-20 00:14:14 -0500

    Added basic output to main.py

--- Python/main.py
+++ Python/main.py
@@ -23,18 +23,22 @@ def streamRunfiles(genNumber, bestList):
 
 def _main():
   global CURRENT_GEN_NUMBER, SURVIVORS_PER_GEN
+  print("Initializing...")
   while True:
     # wait for generation to finish
+    print("Waiting for generation " + CURRENT_GEN_NUMBER + " to finish.")
     while not genDoneRunning(CURRENT_GEN_NUMBER):
       sleep(0.01)
     # sleep little bit longer to avoid race conditions
     # with workers who still may be writing to Results files
     sleep(0.1)
     
+    print("Getting the best runs from generation " + CURRENT_GEN_NUMBER + ".")
     bestRuns = getBestRuns(CURRENT_GEN_NUMBER, SURVIVORS_PER_GEN)
     
     nextGenRuns = []
     # Genetic spawns a list of new runs based on the best from this gen
+    print("Creating new runs...")
     for goodRun in streamRunfiles(CURRENT_GEN_NUMBER, bestRuns):
       nextGenRuns += Genetic.mutate_batch(goodRun, RUNS_PER_GEN / SURVIVORS_PER_GEN)
     
@@ -42,10 +46,12 @@ def _main():
     CURRENT_GEN_NUMBER += 1
     makeGen(CURRENT_GEN_NUMBER)
     
-    # write all new runs to sperate files
+    print("Writing new runs...")
+    # write all new runs to separate files
     for runNum, runFrames in enumerate(nextGenRuns):
       writeRun(CURRENT_GEN_NUMBER, runNum, '\n'.join(map(str, runFrames)))
-      
+    
+    print("Done writing runs.")
     # ...and loop
   
   
