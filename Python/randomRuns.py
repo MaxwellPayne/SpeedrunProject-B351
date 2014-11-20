1e9995c64aaab64fec1de72e940ee7d9e41ccf18
unknown <pkusisto@indiana.edu>
2014-11-20 00:15:25 -0500

    Updated randomRuns uses Genetic.py

--- Python/randomRuns.py
+++ Python/randomRuns.py
@@ -1,17 +1,9 @@
-import random
 import os
+from Genetic import *
+from fileIO import *
 
-def buildline():
-    line = ''
-    for button in range(12):
-        button = random.choice('A.')
-        line += button
-    return line
-
-def buildRuns():
+def randomRuns():
     for runNumber in range(100):
-        lineList = ['|..|' + buildline() + '|............|\n' for line in range(2000)]
-        dir = os.path.dirname(__file__)
-        filename = os.path.join(dir, "../Output/gen0/Runs/run" + str(runNumber) + ".txt")
-        runFile = open(filename, 'w')
-        runFile.writelines(lineList)
+        writeRun(0,runNumber,[str(random_segment()) + '\n' for line in range(2000)])
+
+randomRuns()
