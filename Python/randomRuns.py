a323b322ca35b004b43a8f27914b38f7aa37f690
unknown <pkusisto@indiana.edu>
2014-11-21 02:04:19 -0500

    Ready for a new set of tests with right and X held.

--- Python/randomRuns.py
+++ Python/randomRuns.py
@@ -8,6 +8,6 @@ def randomRuns(runLength):
         
 def simpleRuns(runLength):
     for runNumber in range(100):
-        writeRun(0,runNumber,['|..|...r........|............|\n' for line in range(runLength)])
+        writeRun(0,runNumber,['|..|...R....X...|............|\n' for line in range(runLength)])
 
 simpleRuns(5000)
