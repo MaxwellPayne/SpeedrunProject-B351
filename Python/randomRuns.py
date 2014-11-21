bea9ddbe507876c1e68e03534aae5cbd5b52ffcb
unknown <pkusisto@indiana.edu>
2014-11-20 19:13:33 -0500

    Added simpleRuns() and added a variable for run length.

--- Python/randomRuns.py
+++ Python/randomRuns.py
@@ -2,8 +2,12 @@ import os
 from Genetic import *
 from fileIO import *
 
-def randomRuns():
+def randomRuns(runLength):
     for runNumber in range(100):
-        writeRun(0,runNumber,[str(random_segment()) + '\n' for line in range(2000)])
+        writeRun(0,runNumber,[str(random_segment()) + '\n' for line in range(runLength)])
+        
+def simpleRuns(runLength):
+    for runNumber in range(100):
+        writeRun(0,runNumber,['|..|...r........|............|\n' for line in range(runLength)])
 
-randomRuns()
+simpleRuns(5000)
